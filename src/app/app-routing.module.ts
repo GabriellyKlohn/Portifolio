8791d0161460c639f370c0714008797f12583cde
Fabricio Reck Heidrich <fabricioreckheidrich@gmail.com>
2024-07-02 21:39:59 -0300

    piadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PiadasComponent } from './pages/repositório/piadas/piadas.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'portfolio',pathMatch:'full'},{path:'portfolio',component:PortfolioComponent}
+  {path:'',redirectTo:'portfolio',pathMatch:'full'},
+  {path:'portfolio',component:PortfolioComponent},
+  {path:'piadas',component:PiadasComponent}
 ];
 
 @NgModule({
